613b4801ad83117eaddbe3ad45d4db75f19e1d59
Shane D Wilson <me@shanedwilson.com>
2018-12-15 11:18:21 -0600

    styled listings

--- src/components/ListingItem/ListingItem.js
+++ src/components/ListingItem/ListingItem.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import listingShape from '../../helpers/propz/listingShape';
 
 import './ListingItem.scss';
 
 class ListingItem extends React.Component {
+  static propTypes = {
+    listing: listingShape,
+  }
+
   render() {
     const { listing } = this.props;
     return (
-      <h2>{listing.address}</h2>
+      <li className="listing-item text-center">
+      <span clasName="col-7">{listing.address}</span>
+      <span className="col-3">{listing.price}</span>
+      </li>
     );
   }
 }
