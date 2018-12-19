cbb51a23c2fa5fa9e763f124cc2cb5f5ec7f3e99
Shane D Wilson <me@shanedwilson.com>
2018-12-18 19:48:14 -0600

    input is updating state

--- src/components/ListingForm/ListingForm.js
+++ src/components/ListingForm/ListingForm.js
@@ -1,19 +1,54 @@
 import React from 'react';
 import './ListingForm.scss';
 
+const defaultListing = {
+  address: '',
+  squareFootage: 0,
+  price: 0,
+  numBeds: 0,
+  numBaths: 0,
+  heating: '',
+  cooling: '',
+  imageUrl: '',
+  uid: '',
+};
+
 class ListingForm extends React.Component {
+  state = {
+    newListing: defaultListing,
+  }
+
+  formFieldStringState = (name, e) => {
+    e.preventDefault();
+    const tempListing = { ...this.state.newListing };
+    tempListing[name] = e.target.value;
+    this.setState({ newListing: tempListing });
+  }
+
+  // formFieldNumberState = (name, e) => {
+  //   const tempListing = { ...this.state.newListing };
+  //   tempListing[name] = e.target.value * 1;
+  //   this.setState({ newListing: tempListing });
+  // }
+
+  addressChange = e => this.formFieldStringState('address', e);
+
   render() {
+    const { newListing } = this.state;
     return (
       <div className="listing-form col">
         <h2>Add New Listing</h2>
         <form>
           <div className="form-group">
-            <label for="address">Address:</label>
+            <label htmlFor="address">Address:</label>
             <input
             type="text"
             className="form-control" id="address"
             aria-describedby="addressHelp"
-            placeholder="123 Main St. Nashville, TN 37216"/>
+            placeholder="123 Main St. Nashville, TN 37216"
+            value={newListing.address}
+            onChange={this.addressChange}
+            />
           </div>
           <button type="submit" className="btn btn-danger">Save Listing</button>
         </form>
